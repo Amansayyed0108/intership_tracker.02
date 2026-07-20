0b1392e09456e19ab5a0a62244ec09640337a6b2
Amansayyed0108 <aman.sayyed@autoliv.com>
2026-07-20 11:41:29 +0530

    Refactor app.py for improved structure and functionality

--- app.py
+++ app.py
@@ -231,4 +231,9 @@ def openexcel():
         return jsonify({"status": "error", "message": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    port = int(os.environ.get("PORT", 10000))
+
+app.run(
+    host="0.0.0.0",
+    port=port
+)
